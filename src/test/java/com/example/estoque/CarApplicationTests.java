8c94b9c14f81ea51b730abe193f6bac9ef394170
Pedro Silva <arbeit.pedro.silva@gmail.com>
2023-09-28 12:47:56 -0300

    rename cardapio to estoque

--- src/test/java/com/example/estoque/CarApplicationTests.java
+++ src/test/java/com/example/estoque/CarApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CardapioApplicationTests {
+class CarApplicationTests {
 
 	@Test
 	void contextLoads() {
